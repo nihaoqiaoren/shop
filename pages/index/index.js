78736cabbe7d0d2a68a0ac2873e7a968d22e1188
qiaoren <nihaoqiaoren@163.com>
2019-07-14 08:50:33 +0800

    返回顶部(到200rpx出现)实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,26 +4,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swipers:[],
-    inlets:[],
+    swipers: [],
+    inlets: [],
     floor: [],
+    showTop: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.getSwiper(),
-    this.getinlet(),
-    this.getfloorData()
+      this.getinlet(),
+      this.getfloorData()
   },
   // 封装轮播图
-  getSwiper(){
+  getSwiper() {
     wx.request({
-      url:'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      success:res=>{
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: res => {
         // console.log(res)
-        const message=res.data;
+        const message = res.data;
         this.setData({
           swipers: message
         });
@@ -31,14 +32,14 @@ Page({
     });
   },
   //封装入口函数
-  getinlet(){
+  getinlet() {
     wx.request({
-      url:'https://api.zbztb.cn/api/public/v1/home/catitems',
-      success:res=>{
-      const message=res.data;
-      this.setData({
-        inlets:message
-      })
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: res => {
+        const message = res.data;
+        this.setData({
+          inlets: message
+        })
       }
     })
   },
@@ -56,52 +57,82 @@ Page({
       }
     })
   },
+
+  //点击返回顶部的事件
+  goTop(e) {
+    // console.log(e)
+    const {
+      top
+    } = e.currentTarget.dataset
+    // console.log(top)
+    // 调用返回顶部
+    wx.pageScrollTo({
+      scrollTop: top,
+      duration: 300
+    })
+  },
+
+  //页面滚动触发的函数
+  onPageScroll(event) {
+    const {
+      scrollTop
+    } = event;
+    if (scrollTop > 200) {
+      this.setData({
+        showTop: false
+      })
+    }else{
+      this.setData({
+        showTop: true
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
