31856c88b454ad86b4456648115cc89a6c71e8e6
qiaoren <nihaoqiaoren@163.com>
2019-07-16 17:23:33 +0800

    占位图片+传的4个参数 ID 页数 条数

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,8 +1,6 @@
-const {request} = require("../../utils/request.js");
-
+const { request } = require("../../utils/request.js");
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -10,87 +8,76 @@ Page({
     // 商品列表关键词
     keyword: "",
     activeIndex: 0,
-    goods: []
+    goods: [],
+    pagenum: 1,
+    pagesize: 20
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 在页面生命周期函数中，可以通过 options 获取页面传递的参数
     console.log(options);
-    const {
-      keyword
-    } = options;
-
+    const { keyword, cid } = options;
+    const { pagenum, pagesize } = this.data;
     this.setData({
-      keyword
+      keyword,
+      cid
+
     });
 
-    this.getListData(keyword);
+    this.getListData({query: keyword, cid, pagenum, pagesize});
   },
   // 获取列表数据
-  getListData(keyword) {
+  getListData(params) {
     request({
       url: "goods/search",
       data: {
-        query: keyword
+        ...params
       }
-    })
-      .then(res => {
-        // console.log(res);
-        const { goods } = res;
-        this.setData({
-          goods
-        })
-      })
+    }).then(res => {
+      // console.log(res);
+      const { goods } = res;
+      this.setData({
+        goods
+      });
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
     console.log("onUnload");
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
